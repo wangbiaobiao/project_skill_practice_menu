eb70cc9b43d2c2a1389e4f1c3c4be77fd416eac8
wang biaobiao <wang_shengbiao@qq.com>
2015-03-25 06:09:27 -0700

    Find what varies and encapsulate it

--- menu.c
+++ menu.c
@@ -1,31 +1,59 @@
 #include<stdio.h>
-void help()
+
+#define CMD_NUMBER 256
+
+typedef struct DataNode
+{
+	char* cmd;
+	char* desc;
+	int (*handler)();
+	struct DataNode *next;
+}tDataNode;
+
+int help()
 {
 	printf("this is help cmd\n");
+	return 0;
 }
-void quit()
+
+int quit()
 {
 	printf("this is quit cmd\n");
+	return 0;
 }
+
+static tDataNode head[] =
+{
+	{"help", "this is help cmd", help, &head[1]},
+	{"version","menu program v1.0", NULL, NULL}
+
+};
+
 int main()
 {
-	char cmd[256];
+	char cmd[CMD_NUMBER];
+	tDataNode *p = head;
+	printf("please input your cmd\n");
 	while(1)
 	{
+		tDataNode *p = head;
 		scanf("%s",cmd);
-		if(strcmp(cmd,"help") == 0)
+		while(p != NULL)
 		{
-			help();
-		}
-		else if(strcmp(cmd, "quit") == 0)
-		{
-			quit();
-		}
-		else
-		{
-			printf("this is not a cmd\n");
+			if(strcmp(p->cmd,cmd) == 0)
+			{
+				if(p->handler != NULL)
+				{
+					p->handler();
+				}
+				break;
+			}
+			p = p->next;
+			if(p == NULL)
+			{
+				printf("this isn't a cmd\n");
+			}
 		}
 	}
 	return 0;
-
 }
